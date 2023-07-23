18beca481b28b866f101dd53150330a44845c1b5
Muhammad Zohair <zohair739@gmail.com>
2023-07-23 05:02:26 +0500

    FEATURE: kotlin singly structure created

--- src/kotlin_lang/lists/Collection.kt
+++ src/kotlin_lang/lists/Collection.kt
@@ -5,12 +5,19 @@ import java.util.function.Predicate
 
 interface Collection<T> {
     fun size(): Int
+
     fun add(var1: T): Boolean
+
     fun addAll(var1: Collection<out T>?): Boolean
+
     operator fun contains(var1: T): Boolean
+
     fun containsAll(var1: Collection<*>?): Boolean
+
     fun remove(var1: Any?): Boolean
+
     fun removeAll(var1: Collection<*>?): Boolean
+
     fun removeIf(filter: Predicate<in T>): Boolean {
         Objects.requireNonNull(filter)
         var removed = false
@@ -25,10 +32,16 @@ interface Collection<T> {
     }
 
     operator fun get(index: Int): T
+
     operator fun set(index: Int, element: T): T
+
     fun peek(): T
+
     operator fun iterator(): MutableIterator<T>
+
     fun clear()
+
     override fun equals(var1: Any?): Boolean
+
     fun copyToString(): String?
 }
--- src/kotlin_lang/lists/singly_linked_list/SinglyLinkedList.kt
+++ src/kotlin_lang/lists/singly_linked_list/SinglyLinkedList.kt
@@ -1,4 +1,22 @@
 package kotlin_lang.lists.singly_linked_list
 
-class SinglyLinkedList {
+class SinglyLinkedList<T> : Collection<T> {
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
 }
